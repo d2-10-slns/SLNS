bd27f2ec33bb5a7cf987d83ca97f74931159ea16
lts25 <42810743+lts25@users.noreply.github.com>
2019-03-12 20:42:20 -0500

    I do html for them all
    even made a home page for my dog

--- communication code/new udp server.c
+++ communication code/new udp server.c
@@ -1,14 +1,14 @@
-#include &lt;sys/socket.h&gt;
-#include &lt;netinet/in.h&gt;
-#include &lt;arpa/inet.h&gt;
-#include &lt;stdio.h&gt;
-#include &lt;stdlib.h&gt;
-#include &lt;string.h&gt;
-#include &lt;err.h&gt;
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <err.h>
 
 int main(int argc, char *argv[]) {
 
-	if(argc &lt; 2) { 
+	if(argc < 2) { 
 //		if(daemon(0,0) == -1) err(1,NULL);
 	}
 
@@ -18,17 +18,17 @@ int main(int argc, char *argv[]) {
 	struct sockaddr_in from;
 
 	sock = socket(AF_INET, SOCK_DGRAM, 0);
-	if (sock &lt; 0)
+	if (sock < 0)
 	printf("Can not create socket in server\n");
 
-	memset(&amp;server, 0, sizeof(struct sockaddr_in));
+	memset(&server, 0, sizeof(struct sockaddr_in));
 	server.sin_family = AF_INET;
 	server.sin_port = htons(5005);
 	server.sin_addr.s_addr = INADDR_ANY;
 
-	if(bind(sock, (struct sockaddr *)&amp;server, sizeof(server)) &lt; 0)
+	if(bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
 	printf("Can not bind in server!\n");
-	memset(&amp;from, 0, sizeof(struct sockaddr_in));
+	memset(&from, 0, sizeof(struct sockaddr_in));
 	fromlen = sizeof(struct sockaddr_in);
 
 
@@ -38,17 +38,17 @@ int main(int argc, char *argv[]) {
 		unsigned char outBuf[300];
 //		fflush(stdout);
 
-		n = recvfrom(sock, tBuf, 300, 0, (struct sockaddr*) &amp;from, &amp;fromlen);
-		if (n &lt; 0) {
+		n = recvfrom(sock, tBuf, 300, 0, (struct sockaddr*) &from, &fromlen);
+		if (n < 0) {
 			printf("Can not receive in server!\n");
 		}
 
-		printf("%s from IP:%s, Port:%hu\r\n",&amp;tBuf[0],inet_ntoa(from.sin_addr), ntohs(from.sin_port));
+		printf("%s from IP:%s, Port:%hu\r\n",&tBuf[0],inet_ntoa(from.sin_addr), ntohs(from.sin_port));
 
 		strcpy(outBuf,"127 127 0 0\n\0");
 		socklen_t length = sizeof(struct sockaddr_in);	
-		n = sendto(sock, tBuf, 64, 0, (const struct sockaddr *)&amp;from, fromlen);
-		if(n &lt; 0) {
+		n = sendto(sock, tBuf, 64, 0, (const struct sockaddr *)&from, fromlen);
+		if(n < 0) {
 			printf("Can not send from client");
 		}
 
